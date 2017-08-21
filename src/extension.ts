1e0c0375e63fd059cee466653ed93b5c7eca764d
tolga <tolga.sofuoglu@theappbusiness.com>
2017-08-21 02:28:17 +0100

    it works

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,23 @@
 import {window, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
 var path = require('path');
 var getGitBranchName = require('git-branch-name');
+var opn = require('opn');
 
 export function activate(ctx: ExtensionContext) {
 
-    let wordCounter = new WordCounter();
+    let wordCounter = new JiraLink();
     let controller = new WordCounterController(wordCounter);
 
     ctx.subscriptions.push(controller);
     ctx.subscriptions.push(wordCounter);
 }
 
-export class WordCounter {
+export class JiraLink {
 
     private _statusBarItem: StatusBarItem;
+    private _urlCommand: Disposable;
 
-    public updateWordCount() {
+    public updateJiraLink() {
         
         if (!this._statusBarItem) {
             this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
@@ -30,29 +32,53 @@ export class WordCounter {
         getGitBranchName(
             path.resolve(__dirname, '../../'), 
             (err, branchName) => {
-                var jiraUrl = this.getJiraUrl(branchName);
-                this._statusBarItem.text = `$(tag) ${jiraUrl}`;
+                var jiraStory = this.getJiraStory(branchName);
+                if (jiraStory.Name.length === 0) {
+                    this._statusBarItem.hide();
+                    return;
+                }
+
+                this._urlCommand = commands.registerCommand('extension.jiraLinkCommand', function () {
+                    opn(jiraStory.Url);
+                });
+
+                this._statusBarItem.command = "extension.jiraLinkCommand";
+                this._statusBarItem.text = `$(tag) ${jiraStory.Name}`;
                 this._statusBarItem.show();
             });
     }
 
-    public getJiraUrl(branchName: string) : string {
-        return branchName;
+    public getJiraStory(branchName: string) : JiraStory {
+        var namePattern = /feature\/(.*)\/.*/g;
+        var match = namePattern.exec("feature/PROJ-123/test");
+        while(match === null) {
+            return { Name: "", Url: "" };
+        }
+        var storyNumber = match[1];
+        var jiraUrl = `https://myjirahost.atlassian.net/browse/${storyNumber}`;
+
+        return { Name: storyNumber, Url: jiraUrl };;
     }
 
     public dispose() {
         this._statusBarItem.dispose();
+        this._urlCommand.dispose();
     }
+}
+
+class JiraStory {
+    public Url: string;
+    public Name: string;
 }
 
 class WordCounterController {
 
-    private _wordCounter: WordCounter;
+    private _jiraLink: JiraLink;
     private _disposable: Disposable;
 
-    constructor(wordCounter: WordCounter) {
-        this._wordCounter = wordCounter;
-        this._wordCounter.updateWordCount();
+    constructor(jiraLink: JiraLink) {
+        this._jiraLink = jiraLink;
+        this._jiraLink.updateJiraLink();
 
         let subscriptions: Disposable[] = [];
         window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
@@ -62,7 +88,7 @@ class WordCounterController {
     }
 
     private _onEvent() {
-        this._wordCounter.updateWordCount();
+        this._jiraLink.updateJiraLink();
     }
 
     public dispose() {
