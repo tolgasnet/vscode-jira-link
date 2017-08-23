d0ea8426150c30d9aab44ef08fdfacb009d316eb
tolga <tolga.sofuoglu@theappbusiness.com>
2017-08-23 11:32:42 +0100

    refactored controller

--- src/jiralink-controller.ts
+++ src/jiralink-controller.ts
@@ -4,41 +4,27 @@ import { JiraLink } from './jiralink';
 export class JiraLinkController {
     
         private _jiraLink: JiraLink;
-        private _disposable: Disposable;
+        private _subscriptions: Disposable[] = [];
     
         constructor(jiraLink: JiraLink) {
             this._jiraLink = jiraLink;
             this._jiraLink.updateJiraLink();
     
-            let subscriptions: Disposable[] = [];
-            window.onDidChangeActiveTextEditor(this._onActiveEditorChangedEvent, this, subscriptions);
-    
-            var clickUrlCommand = commands.registerCommand('extension.jiraBrowseLinkCommand', this._onClickJiraLinkEvent, this);
-    
-            var setUrlCommand = commands.registerCommand('extension.setJiraBaseUrlCommand', this._onSetBaseUrl, this);
-    
-            var setBranchPatternCommand = commands.registerCommand('extension.setBranchPatternCommand', this._onSetBranchPattern, this);
-    
-            this._disposable = Disposable.from(...subscriptions, clickUrlCommand, setUrlCommand, setBranchPatternCommand);
-        }
-    
-        private _onActiveEditorChangedEvent() {
-            this._jiraLink.updateJiraLink();
-        }
-    
-        private _onClickJiraLinkEvent() {
-            this._jiraLink.openJiraLink();
-        }
-    
-        private _onSetBaseUrl() {
-            this._jiraLink.setBaseUrl();
-        }
-    
-        private _onSetBranchPattern() {
-            this._jiraLink.setBranchPattern();
+            window.onDidChangeActiveTextEditor(this._jiraLink.updateJiraLink, this, this._subscriptions);
+
+            this.registerCommand('extension.jiraBrowseLinkCommand', () => this._jiraLink.openJiraLink());
+
+            this.registerCommand('extension.setJiraBaseUrlCommand', () => this._jiraLink.setBaseUrl());
+
+            this.registerCommand('extension.setBranchPatternCommand', () => this._jiraLink.setBranchPattern());
         }
     
         public dispose() {
-            this._disposable.dispose();
+            Disposable.from(...this._subscriptions).dispose();
+        }
+
+        private registerCommand(command: string, callback: (...args: any[]) => any) {
+            var disposible = commands.registerCommand(command, callback, this);
+            this._subscriptions.push(disposible);
         }
     }
