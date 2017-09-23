24993c760219b2ade0d5d4e3004d5715070d9a32
tolga <tolga.sofuoglu@theappbusiness.com>
2017-09-23 17:46:30 +0100

    modified default branch pattern so that it starts with any word

--- src/default-branch-pattern.ts
+++ src/default-branch-pattern.ts
@@ -1,2 +1,2 @@
 
-export const defaultBranchPattern = /feature\/([a-zA-Z]{1,4}\-?[0-9]{1,4})/i;
+export const defaultBranchPattern = /\w+\/([a-zA-Z]{1,4}\-?[0-9]{1,4})/i;
--- test/default-branch-pattern.test.ts
+++ test/default-branch-pattern.test.ts
@@ -7,7 +7,9 @@ describe("default branch pattern", () => {
     const testCases = [
         { branchName: 'feature/A-1', expectedStoryNumber: 'A-1' },
         { branchName: 'feature/B-1234', expectedStoryNumber: 'B-1234' },
-        { branchName: 'feature/CDEF-5678', expectedStoryNumber: 'CDEF-5678' }
+        { branchName: 'feature/CDEF-5678', expectedStoryNumber: 'CDEF-5678' },
+        { branchName: 'tech/A-1', expectedStoryNumber: 'A-1' },
+        { branchName: 'hotfix/KLM-1234', expectedStoryNumber: 'KLM-1234' }
     ];
 
     it("should accept common patterns", () => {
@@ -19,5 +21,6 @@ describe("default branch pattern", () => {
 
 const verifyBranchPattern = (branchName, expectedStoryNumber) => {
     var match = defaultBranchPattern.exec(branchName);
+    expect(match, `${branchName} could not be parsed. expected ${expectedStoryNumber}`).not.to.be.null;
     expect(match[1]).to.equal(expectedStoryNumber);
 };
